1b0f742a56319ba86a1d8a9292cfd5e8157c0377
Jacob S. Steward <jacobtheeldest@gmail.com>
2026-04-07 22:00:14 -0400

    feat(build): add sshfs package

--- build/10-build.sh
+++ build/10-build.sh
@@ -45,7 +45,7 @@ echo "::endgroup::"
 echo "::group:: Install Packages"
 
 # Install packages using dnf5
-# Example: dnf5 install -y tmux
+dnf5 install -y sshfs
 
 # Example using COPR with isolated pattern:
 # copr_install_isolated "ublue-os/staging" package-name
